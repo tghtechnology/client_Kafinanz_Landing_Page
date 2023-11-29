c8aa3b0cd36d2377e45f22f1c523fb6c739adb3a
William Castro <wcastroh07@gmail.com>
2023-11-29 15:00:28 -0500

    quitando estilos de css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily : {
-      'poppins': ['Poppins', 'sans-serif']
-    },
-    colors: {
-      'pink-light': '#E00CBD',
-      'blue-dark': '#0847AD',
-      'pink-gray': '#A63884',
-      'orange-ligth': '#EA3D1A',
-      'yellow-dark':'#FFA401',
-      'indigo-black': '#37298C',
-      'purple-black': '#5C0742',
-      'white': '#FFFFFF'
-    },
-    screens: {
-      '1xl': '1440px',
+    extend: {
+      fontFamily : {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'pink-light': '#E00CBD',
+        'blue-dark': '#0847AD',
+        'pink-gray': '#A63884',
+        'orange-ligth': '#EA3D1A',
+        'yellow-dark':'#FFA401',
+        'indigo-black': '#37298C',
+        'purple-black': '#5C0742',
+      },
+      screens: {
+        '1xl': '1440px',
+        '3xl': '2100px'
+      }
     }
   },
   plugins: [],
