ca5adaa0876e88ee7b47f6b44cd3abd7e6d2b68c
William Castro <wcastroh07@gmail.com>
2023-11-29 10:58:58 -0500

    section servicio contable completado

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,12 @@ export default {
       'purple-black': '#5C0742'
     },
     screens: {
-      '1xl': '1440px'
+      '1xl': '1440px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     }
   },
   plugins: [],
