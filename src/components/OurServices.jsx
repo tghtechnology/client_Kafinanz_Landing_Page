c5c67c82d5a3f5f866adc1d8330eb0b075feff31
MBMAZAM <maza_1506@hotmail.com>
2023-11-30 12:47:06 -0500

    Change

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -7,11 +7,30 @@ const OurServices = () => {
           Ventajas de nuestros servicios
         </h3>
       </div>
-      <section className="grid sm:grid-cols-2 md:grid-cols-4 text-2xl text-center font-semibold text-white md:h-72 md:max-h-72 3xl:max-w-7xl 3xl:mx-auto">
-        <article className="bg-pink-gray grid place-content-center px-3 py-4 md:p-0 h-52 md:h-auto">
-          <p className="md:max-w-[280px]">Copia de seguridad en la nube.</p>
+      <section className="grid sm:grid-cols-2 md:grid-cols-4 text-2xl text-center font-semibold text-white h-full max-h-[800px] md:h-[500px] 3xl:max-w-7xl 3xl:mx-auto px-2 gap-x-4 gap-y-4">
+        <article className="bg-clip-bg bg-gradient-to-b from-[#4C1165] to-[#0847AD] text-transparent grid place-content-center py-4 max-w-[331px] rounded-[110px]">
+          <p className="md:max-w-[280px] text-white">
+            Copia de seguridad en la nube.
+          </p>
         </article>
-        <article className="bg-purple-black grid place-content-center px-3 py-4 md:p-0 h-52 md:h-auto">
+        <article className="bg-clip-bg bg-gradient-to-b from-[#4C1165] to-[#CC0E34] text-transparent grid place-content-center py-4 max-w-[331px] rounded-[110px]">
+          <p className="md:max-w-[280px] text-white">
+            Acceso a documentos originales en todo momento.
+          </p>
+        </article>
+        <article className="bg-clip-bg bg-gradient-to-b from-[#4C1165] via-[#8E077D] to-[#FFA401] text-transparent grid place-content-center py-4  max-w-[331px] rounded-[110px]">
+          <p className="md:max-w-[280px] text-white">
+            Ahorro en costos al no contratar personal contable fijo.
+          </p>
+        </article>
+        <article className="bg-clip-bg bg-gradient-to-b from-[#0847AD]  to-[#4C1165] text-transparent grid place-content-center py-4 max-w-[331px] rounded-[110px]">
+          <p className="md:max-w-[280px] text-white">
+            Enfoque en tus actividades comerciales mientras nosotros manejamos
+            la contabilidad.
+          </p>
+        </article>
+        #0847AD, #4C1165
+        {/* <article className="bg-purple-black grid place-content-center px-3 py-4 md:p-0 h-52 md:h-auto">
           <p className="md:max-w-[280px]">
             Acceso a documentos originales en todo momento.
           </p>
@@ -26,7 +45,7 @@ const OurServices = () => {
             Enfoque en tus actividades comerciales mientras nosotros manejamos
             la contabilidad.
           </p>
-        </article>
+        </article> */}
       </section>
       {/* <div className="max-w-full h-6 bg-[#DFC2D6]"></div> */}
     </div>
