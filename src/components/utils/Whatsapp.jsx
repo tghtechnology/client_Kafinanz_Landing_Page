b50e9e12309d6a03e238dbdb050cfa08cac6b454
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-12-07 01:21:34 -0500

    cambios del botom de whatsapp

--- src/components/utils/Whatsapp.jsx
+++ src/components/utils/Whatsapp.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 
 const Whatsapp = () => {
-  const [showMessage, setShowMessage] = useState(false);
+  const messageDefault = "Hola, Necesito tu servicio de consultoría digital 📱📡📊";
+  const [showMessage, setShowMessage] = useState(true);
 
-  const handleWhatsAppClick = () => {
-    const mensajePredeterminado = encodeURIComponent(
-      "Hola, Necesito tu servicio de consultoría digital 📱📡📊"
-    );
-    const url = `https://api.whatsapp.com/send?phone=987735564&text=${mensajePredeterminado}`;
-    window.open(url, "_blank");
-  };
+  // const handleWhatsAppClick = () => {
+  //   const mensajePredeterminado = encodeURIComponent(
+  //     "Hola, Necesito tu servicio de consultoría digital 📱📡📊"
+  //   );
+  //   const url = `https://api.whatsapp.com/send?phone=965441168&text=${mensajePredeterminado}`;
+  //   window.open(url, "_blank");
+  // };
 
   return (
     <>
-      <button className="fixed  transition-transform transform hover:scale-110 animate-bounce animate-duration-2000 animate-iteration-3 animate__animated animate__infinite z-50"
+      {/* <button className="fixed  transition-transform transform hover:scale-110 animate-bounce animate-duration-2000 animate-iteration-3 animate__animated animate__infinite z-50"
         style={{ bottom: "4rem", right: "2rem", animationName: "scaleIn" }}
         onClick={handleWhatsAppClick}
         onMouseEnter={() => setShowMessage(true)}
@@ -27,10 +28,26 @@ const Whatsapp = () => {
       </button>
 
       {showMessage && (
-        <div className="fixed bottom-16 right-24 bg-green-500 py-3 px-9 text-white font-semibold rounded-bl-full rounded-tr-full text-sm md:text-lg- shadow-lg z-50">
+        <div className="fixed bottom-[4.5rem] right-24 bg-green-500 py-1.5 px-9 font-normal rounded-bl-full rounded-tr-full text-sm md:text-lg- shadow-lg z-50">
           Chatea con nosotros
         </div>
-      )}
+      )} */}
+
+      <a
+        href={`https://api.whatsapp.com/send?phone=965441168&text=${messageDefault}`}
+        className="fixed w-16 h-16 bg-green-500 bottom-16 right-4 rounded-full flex items-center justify-center transition-transform transform hover:scale-110 animate-bounce"
+        style={{ animationName: "scaleIn" }}
+      >
+        <img
+          className=" w-11 h-11"
+          src="/image/whatsapp.svg"
+          alt="logo whatsapp"
+        />
+      </a>
+      {showMessage && 
+        <div className="hidden fixed bottom-20 right-24 bg-green-500 py-1 px-3 rounded-bl-full rounded-tr-full shadow-lg lg:block">
+          <p>Chatea con nosotros</p>
+        </div>}
     </>
   );
 };
