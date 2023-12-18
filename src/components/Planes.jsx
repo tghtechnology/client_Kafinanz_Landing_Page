0d52aefb55f146b86322171c0023e9e21752da80
christopher-cuadros <110701105+visual-junior@users.noreply.github.com>
2023-12-18 09:48:03 -0500

    detalles

--- src/components/Planes.jsx
+++ src/components/Planes.jsx
@@ -5,7 +5,8 @@ const Planes = () => {
         <h1 className="text-center uppercase font-bold text-3xl bg-clip-text bg-gradient-to-l from-[#c9c762] to-[#eca305] text-transparent mb-8">
           ¿Ya conocias nuestros planes?
         </h1>
-        <div className="relative grid gap-4 text-white md:grid-cols-planes-desktop place-content-center md:gap-14 px-5">
+        <div className="relative grid gap-2 text-white md:grid-cols-planes-desktop place-content-center md:gap-10 px-5 py-5 ">
+
           <div className="bg-[url('/image/plan-mensual.webp')] bg-no-repeat bg-cover rounded-[70px] h-[334px] max-h-[334px] min-h-[220px] lg:h-[667px] lg:max-h-[600px] flex flex-col justify-end py-8 px-4 lg:px-5 gap-3 lg:py-20">
             <h2 className="font-bold text-base lg:text-3xl  uppercase w-max after:content-[' '] after:block after:w-full after:h-1 after:bg-white after:rounded-md">
               Mensual
@@ -17,6 +18,7 @@ const Planes = () => {
               <li>Registro contable</li>
             </ul>
           </div>
+
           <div className="bg-[url('/image/plan-anual.webp')] bg-no-repeat bg-cover rounded-[70px] h-[334px] max-h-[334px] min-h-[220px] lg:h-[667px] lg:max-h-[600px] flex flex-col justify-end py-8 px-4 lg:px-5 gap-3 lg:py-20">
             <h2 className="font-bold text-base lg:text-3xl uppercase w-max after:content-[' '] after:block after:w-full after:h-1 after:bg-white after:rounded-md">
               Anual
@@ -29,7 +31,8 @@ const Planes = () => {
               </li>
             </ul>
           </div>
-          <div className="bg-[url('/image/plan-cotizacion.webp')] bg-no-repeat bg-cover rounded-[70px] h-[434px] max-h-[434px] lg:h-[667px] lg:max-h-[600px] flex flex-col justify-end lg:pb-[108px]  py-8 px-4 lg:px-5 gap-3 lg:py-20">
+
+          <div className="bg-[url('/image/plan-cotizacion.webp')] bg-no-repeat bg-cover rounded-[70px] h-[434px] max-h-[434px] md:min-h-[500px] lg:h-[667px] lg:max-h-[600px] flex flex-col justify-end lg:pb-[108px]  py-8 px-4 lg:px-5 gap-3 lg:py-20">
             <h2 className="font-bold text-base lg:text-3xl uppercase after:content-[' '] after:block after:w-full after:h-1 after:bg-white after:rounded-md">
               Cotización de servicio contable
             </h2>
@@ -45,8 +48,9 @@ const Planes = () => {
               </li>
             </ul>
           </div>
+
           <div className="bg-[url('/image/plan-auditoria.webp')] bg-no-repeat bg-cover rounded-[70px] h-[534px] max-h-[494px] lg:h-[667px] lg:max-h-[600px] flex flex-col justify-end py-8 px-4 lg:px-5 gap-3 lg:py-20">
-            <h2 className="text-lg lg:text-3xl font-black uppercase after:content-[' '] after:block after:w-full after:h-1 after:bg-white after:rounded-md">
+            <h2 className="text-lg lg:text-3xl uppercase after:content-[' '] after:block after:w-full after:h-1 after:bg-white after:rounded-md">
               Auditoria Tributaria Preventiva
             </h2>
             <p className="font-bold text-base lg:text-xl">
@@ -61,6 +65,7 @@ const Planes = () => {
               <li>Documentación de Auditoría</li>
             </ul>
           </div>
+
         </div>
       </div>
     </>
